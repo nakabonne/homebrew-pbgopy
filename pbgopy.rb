e8b9e3c711b47164ee98a4b0bca8e2de28210789
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-28 21:40:05 +0900

    Brew formula update for pbgopy version v0.3.0

--- pbgopy.rb
+++ pbgopy.rb
@@ -2,24 +2,24 @@
 class Pbgopy < Formula
   desc "Copy and paste between devices"
   homepage "https://github.com/nakabonne/pbgopy"
-  version "0.2.1"
+  version "0.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/nakabonne/pbgopy/releases/download/v0.2.1/pbgopy_0.2.1_darwin_amd64.tar.gz"
-    sha256 "b8fc51d240d1e896516cbcebe88c4e2eef4633cc59ee868b4a1c765cd28f263d"
+    url "https://github.com/nakabonne/pbgopy/releases/download/v0.3.0/pbgopy_0.3.0_darwin_amd64.tar.gz"
+    sha256 "6c5e16e3502ec341c111a864dff3fdfd807e26ecdf6f67746b80776434445085"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/nakabonne/pbgopy/releases/download/v0.2.1/pbgopy_0.2.1_linux_amd64.tar.gz"
-    sha256 "6e11a07eba4a0108b2f93ef0b150f08a99887357ac5724ba135362315956e10f"
+    url "https://github.com/nakabonne/pbgopy/releases/download/v0.3.0/pbgopy_0.3.0_linux_amd64.tar.gz"
+    sha256 "10e4912497415d3f87812d8d967337210ea8d1d1a2824f7178ccfade576a3cc5"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/nakabonne/pbgopy/releases/download/v0.2.1/pbgopy_0.2.1_linux_armv6.tar.gz"
-    sha256 "e91cf7b8ba2232fc519277f15aca15ac045486f21130be1772008082dbea5000"
+    url "https://github.com/nakabonne/pbgopy/releases/download/v0.3.0/pbgopy_0.3.0_linux_armv6.tar.gz"
+    sha256 "0b552375f5cbf0ca067fbb380bfb3e119cb6117f2beee4b7edeb48501de346a8"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/nakabonne/pbgopy/releases/download/v0.2.1/pbgopy_0.2.1_linux_arm64.tar.gz"
-    sha256 "f7ddadd884627c5befd15b081bc1c4999c505557fef9380a7b313f7e3bf42f2a"
+    url "https://github.com/nakabonne/pbgopy/releases/download/v0.3.0/pbgopy_0.3.0_linux_arm64.tar.gz"
+    sha256 "5204a1e79ef30c46bcf5f7a02274141b3cc6f3395aef9c8957ab4d90c97fe4c0"
   end
 
   def install
